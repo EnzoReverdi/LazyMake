89dfc6e21ad6b3277158dd542d8266f903c2b6ab
EnzoReverdi <enzoreverdi@epitech.eu>
2021-03-01 12:28:33 +0100

    units test support added, color might be next, this or lib makefile generator

--- lazymake.sh
+++ lazymake.sh
@@ -10,7 +10,11 @@ bin_line="SOFTNAME\t=\tbin"
 
 ccflags="CCFLAGS\t=\t-g3 -fno-builtin"
 
-if [[ $check_makefile = "Makefile"  ]];
+max_len=0
+
+idx=0
+
+if [[ $check_makefile = "Makefile"  ]]
 then
     lib_line=$(sed -n 16p Makefile)
     bin_line=$(sed -n 14p Makefile)
@@ -43,12 +47,29 @@ printf "LINKHEADER\t=\t-I./include\n\n" >> Makefile
 
 printf "${ccflags}\n\n" >> Makefile
 
-printf "SRC\t=\t\t\\" >> Makefile
-printf "\n" >> Makefile
+printf "SRC\t=" >> Makefile
+
 for i in $(find src -name '*.c' ); 
 do
-    printf "\t\t$i\t\t\\" >> Makefile
+    len=$((${#i} + 2))
+    if [[ $len -gt $max_len ]]
+    then
+        max_len=$len
+    fi
+done
+
+for i in $(find src -name '*.c' ); 
+do
+    len=${#i}
+    nb_space=$((${max_len} - ${len}))
+    if [[ $idx -eq 0 ]]
+    then
+        printf "   $i% *s" ${nb_space} "\\" >> Makefile
+    else
+        printf "\t\t$i% *s" ${nb_space} "\\" >> Makefile
+    fi
 	printf "\n" >> Makefile
+    idx=$((${idx} + 1))
 done
 printf "\n" >> Makefile
 
@@ -77,6 +98,18 @@ printf "\t@rm -rf \$(OBJ)\n\n" >> Makefile
 
 printf "fclean:\tclean\n" >> Makefile
 printf "\t@make fclean -C lib/my\n" >> Makefile
-printf "\t@rm -rf \$(SOFTNAME)\n\n" >> Makefile
+printf "\t@rm -rf \$(SOFTNAME) *.gcno *.gcda unit_tests\n\n" >> Makefile
 
-printf "re:\tfclean all\n" >> Makefile
+####### unit test part ###
+
+printf "unit_tests:\tre\n" >> Makefile
+printf "\t@gcc -o unit_tests \$(filter-out src/main.c, \$(SRC)) tests/*.c \$(LINKHEADER) \$(LINKLIB) --coverage -lcriterion\n" >> Makefile
+printf "\t@./unit_tests\n\n" >> Makefile
+
+printf "tests_run:\tunit_tests\n\n" >> Makefile
+
+printf "gcovr:\ttests_run\n" >> Makefile
+printf "\t@gcovr --exclude tests/\n" >> Makefile
+printf "\t@gcovr --exclude tests/ --branches\n\n" >> Makefile
+
+printf "re:\tfclean all" >> Makefile
